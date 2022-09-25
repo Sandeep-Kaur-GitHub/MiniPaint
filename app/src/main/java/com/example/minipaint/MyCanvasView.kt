f8bb5b323c2e95b5ced0b63383a2117577ad65c7
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-09-25 18:13:07 +0530

    Implement touchMove() method

--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 private const val STROKE_WIDTH = 12f // has to be float
 class MyCanvasView(context: Context): View(context) {
@@ -18,6 +19,7 @@ class MyCanvasView(context: Context): View(context) {
     private var currentY = 0f
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
     private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
     // Set up the paint with which to draw.
     private val paint = Paint().apply {
         color = drawColor
@@ -61,7 +63,20 @@ class MyCanvasView(context: Context): View(context) {
         currentY = motionTouchEventY
     }
 
-    private fun touchMove() {}
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
 
     private fun touchUp() {}
 }
